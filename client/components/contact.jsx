a3ca6e76db2339f5230aafdc7946469c86fcc677
DanLombardy <lombardy.dan@gmail.com>
2016-05-03 23:38:53 -0700

    Fixed contact lineup bug

--- client/components/contact.jsx
+++ client/components/contact.jsx
@@ -22,11 +22,11 @@ module.exports = React.createClass({
 				<div className='quarterCol tablet-tenth-col inline'></div>
 				<div className='halfCol tablet-eighty-col inline'>
 					<div id="mc_embed_signup">
-						<form action="//siffx.us12.list-manage.com/subscribe/post?u=fcfebfed0a0e10f732ec35daa&amp;id=c5b1f24a76" method="post" id="mc-embedded-subscribe-form"  onChange name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+						<form action="//siffx.us12.list-manage.com/subscribe/post?u=fcfebfed0a0e10f732ec35daa&amp;id=c5b1f24a76" method="post" id="mc-embedded-subscribe-form"  name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
 							<div id="mc_embed_signup_scroll">
 								<label for="mce-EMAIL"></label>
 								<input type="email" value="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="please enter your email address" onChange= { this.handleChange } value={this.state.value} required/>
-								<div id='anti-bot-input' aria-hidden="true"><input type="text" name="b_fcfebfed0a0e10f732ec35daa_c5b1f24a76" tabindex="-1" value="" /></div>
+								<div id='anti-bot-input' aria-hidden="true"><input type="text" name="b_fcfebfed0a0e10f732ec35daa_c5b1f24a76" tabindex="-1" /></div>
 								<div><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
 							</div>
 						</form>
